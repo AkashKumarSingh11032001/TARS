dec83f2a5c4159a98cba2a50c0afde9831075323
Akash <akashkumarsingh11032001@gmail.com>
2023-03-05 18:56:47 +0530

    clone completed

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ app.use(cors())
 const port = 3080
 
 app.post('/', async (req, res) => {
-    const {message} = req.body
+    const {message, currentModel} = req.body
     const response = await openai.createCompletion({
-        model: "text-davinci-003",
+        model: `${currentModel}`,
         prompt: `${message}`,
         max_tokens: 100,
         temperature: 0.5,
