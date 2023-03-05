afdfa08b53c056b02de8a9c6632cedd43996e8a3
Akash <akashkumarsingh11032001@gmail.com>
2023-03-05 12:02:56 +0530

    express set for api

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 //  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
 const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
+
 const configuration = new Configuration({
     organization: "org-12g2sKeWftKJlsqJ54UJHP1j",
     apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "Say this is a test",
-  max_tokens: 7,
-  temperature: 0,
+// const response = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: "Say this is a test",
+//     max_tokens: 7,
+//     temperature: 0,
+// });
+
+
+const app = express()
+const port = 3080
+
+app.post('/', async (req, res) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+    });
+    console.log(response.data.choices[0].text)
+    res.json({
+        data: response.data
+    })
 });
 
-
+app.listen(port, () => {
+    console.log(`Listining at port https://localhost:${port}`)
+});
